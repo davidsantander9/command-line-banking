d62e54c41ce0560ab8251fb23d2148b7f8f149d0
davidsantander9@gmail.com <davidsantander9@gmail.com>
2022-03-09 23:36:16 -0600

    create-acct, show-accts, add-product commands added

--- src/commands/Command.java
+++ src/commands/Command.java
@@ -1,9 +1,13 @@
 package commands;
 
+
+import bank.AdministradorCuentasHabientes;
+import bank.Cliente;
 import handlers.PropertyHandler;
 
 public class Command {
     private static final String PROP_PASSWORD = "system.password";
+    private static AdministradorCuentasHabientes admAccounts = new AdministradorCuentasHabientes();
 
     private Command(){}
     public static void runCommandListener() {
@@ -18,6 +22,15 @@ public class Command {
                 case "chg-pass":
                     changePassword();
                     break;
+                case "create-acct":
+                    createAccountHolder();
+                    break;
+                case "show-accts":
+                    showAccounts();
+                    break;
+                case "add-product":
+                    addProduct();
+                    break;
                 case "exit":
                     break;
                 default:
@@ -29,6 +42,9 @@ public class Command {
     private static void printHelp() {
         System.out.println("- help: shows available commands and short description\n" +
                 "- chg-pass: change user password\n" +
+                "- create-acct: create account holder\n" +
+                "- show-accts: show all accounts \n" +
+                "- create-acct: create account holder\n" +
                 "- exit: finishes the program");
     }
 
@@ -48,6 +64,35 @@ public class Command {
         } else {
             System.err.println("Password could not be set");
         }
+    }
+
+    private static void createAccountHolder(){
+        System.out.print("Enter number client: ");
+        String nameClient = new String(System.console().readLine());
+
+        System.out.print("Enter client name: ");
+        String numClient = new String(System.console().readLine());
+
+        System.out.print("Enter monthlyIncome: ");
+        double monthlyIncome = Double.parseDouble(System.console().readLine());
+
+        Cliente client = new Cliente(nameClient, numClient, monthlyIncome);
+        admAccounts.agregarCuentaHabiente(client);
+    }
+
+    private static void showAccounts(){
+        admAccounts.mostrarInfoCuentasHabiente();
+    }
+
+    private static void addProduct(){
+        admAccounts.mostrarInfoCuentasHabiente();
+        System.out.println();
+        System.out.println("Choose a account to add a product e.g: 3");
+        System.out.print("Enter account: ");
+        int indexAccount = Integer.parseInt(System.console().readLine());
+
+        admAccounts.agregarProducto(indexAccount, AdministradorCuentasHabientes.getTarjetaCredito());
+
     }
 
 }
