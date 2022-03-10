75ee6d27da91610062779300c25f4b69a37d565b
davidsantander9@gmail.com <davidsantander9@gmail.com>
2022-03-09 23:35:08 -0600

    account holder class added

--- src/bank/CuentaHabiente.java
+++ src/bank/CuentaHabiente.java
@@ -1,5 +1,42 @@
 package bank;
 
+import java.util.List;
+
 public class CuentaHabiente {
+    private Cliente cliente;
+    private AdministradorProducto administrador;
+
+    public CuentaHabiente(Cliente cliente, AdministradorProducto administrador) {
+        this.cliente = cliente;
+        this.administrador = administrador;
+    }
+
+    public AdministradorProducto getAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(AdministradorProducto administrador) {
+        this.administrador = administrador;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void agregarProducto(ProductoFinanciero producto){
+        administrador.agregarProducto(cliente, producto);
+    }
+
+    public List<ProductoFinanciero> getProductos(){
+        return administrador.getProductos(cliente.getNumCliente());
+    }
+
+    public boolean puedeCancelar(){
+        return administrador.puedeCancelar(cliente);
+    }
 
 }
